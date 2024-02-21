196b350ed446bd778fc23332a4c1e664ab4e4e6c
Oluwatobi02 <olola73@morgan.edu>
2024-02-21 11:17:14 -0500

    resume updated

--- components/about.tsx
+++ components/about.tsx
@@ -12,11 +12,11 @@ const { ref, inView } = useInView({
   threshold: 0.75,
 });
 
-const { setActiveSection } = useActiveSectionContext();
+const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
 
 useEffect(() => {
-  if (inView) {
+  if (inView && Date.now()- timeOfLastClick > 1000) {
     setActiveSection("About");
   
   }
